27aa998589b8258f9fd118679e7a89ca1306ab1a
PatrickGTR <patricksubang@live.com>
2019-06-10 15:52:21 +0100

    added simple !host and !hostmsg cmd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands import CommandNotFound
 import os
 
 client = commands.Bot(command_prefix = "!")
@@ -9,13 +10,47 @@ async def on_ready():
     await client.change_presence(activity=discord.Game("Made with ❤️ by PatrickGTR"))
     print('Logged on as', client.user)
 
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, CommandNotFound):
+        return
+    raise error
 
 @client.command()
-async def host(ctx, code, mode):
+async def hostmsg(ctx, *, message=None) :
+    await ctx.message.delete() # delete the command message.
+
+    if(message == None) :
+        await ctx.send(":exclamation: !hostmsg <message>")
+        return # disallow the code from continuing below
+
+    embed = discord.Embed(
+        title = f"Host Message",
+        colour = discord.Colour.orange() # the embed colour
+    )
+
+    host = ctx.message.author # person who wrote the command
+    embed.add_field(name="**Host**", value=host, inline=True)
+    embed.add_field(name="**Message**", value=message, inline=True)
+    await ctx.send(embed=embed) # send the message
+
+
+@client.command()
+async def host(ctx, code=None, mode=None) :
+
+    await ctx.message.delete() # delete the command message.
+
+    if(code == None):
+        await ctx.send(":exclamation: !host <custom_key> <solo or duo>")
+        return
+
+    if(mode not in ["solo", "duo", "Solo", "Duo"]):
+        await ctx.send(":exclamation: !host <custom_key> <solo or duo>")
+        return # disallow the code from continuing below
 
     embed = discord.Embed(
         title = f"{mode.upper()} Arena",
-        colour = discord.Colour.orange()
+        colour = discord.Colour.orange() # the embed colour
     )
 
     rules = \
@@ -24,7 +59,7 @@ async def host(ctx, code, mode):
     **DO NOT** fight after clearing out and leaving your drop
     **DO NOT** fight until the 2nd zone has fully closed
     **DO NOT** grief, this means stealing any item at all
-    **DO NOT** play like a bot, we want you to practice like this is WCn
+    **DO NOT** play like a bot, we want you to practice like this is WC
     Game will start in **3 minutes** from this post
     """
 
@@ -33,10 +68,10 @@ async def host(ctx, code, mode):
     embed.add_field(name="**Host**", value=host)
     embed.add_field(name="**Custom Key**", value=code, inline=False)
     embed.add_field(name="**Rules**", value=rules, inline=False)
+    embed.set_footer(text="Please react below once you're ready!")
 
-    channel = ctx.message.channel # retrieve the channel
-    await ctx.message.delete()
-    await channel.send(embed=embed) # send the message
+    msg = await ctx.send(embed=embed) # send the message
+    await msg.add_reaction('👍') # attach reaction to message
 
 @client.command()
 async def kill(ctx):
@@ -44,3 +79,5 @@ async def kill(ctx):
     await client.logout()
 
 client.run(os.getenv("TOKEN"))
+
+
